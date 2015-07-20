12cfbb8a29b822e0c78c8183c526190c7b051714
milezoom <mu51im.x28@gmail.com>
2015-07-20 17:48:43 +0700

    update beberapa file buat update spreadsheet

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -23,29 +23,41 @@ class Testing extends CI_Controller
         /*-------------------------- get list spreadsheet on your drive -----------------------------*/
         $spreadsheetList = $spreadsheetAPI->getSpreadsheetList();
         $spreadsheetIds = array_keys($spreadsheetList);
-        $data['sslist'] = $spreadsheetList;
+        //$data['sslist'] = $spreadsheetList;
         /*-------------------------------------------------------------------------------------------*/
 
         /*----------------------- get list worksheet on first spreadsheet ---------------------------*/
         $worksheetList = $spreadsheetAPI->getWorksheetList($spreadsheetIds[0]);
         $worksheetIds = array_keys($worksheetList);
-        $data['wslist'] = $worksheetList;
+        //$data['wslist'] = $worksheetList;
         /*-------------------------------------------------------------------------------------------*/
 
         /*---------------------- get raw data feed from selected worksheet --------------------------*/
         $worksheetDataList = $spreadsheetAPI->getWorksheetDataList($spreadsheetIds[0], $worksheetIds[0]);
-        $data['wsdlist'] = $worksheetDataList;
+        //$data['wsdlist'] = $worksheetDataList;
         /*-------------------------------------------------------------------------------------------*/
 
         /*------------------- get data feed from cell range selected worksheet ----------------------*/
-        $cellRange = ['columnStart' => 1, 'columnEnd' => 1, 'rowStart' => 1, 'rowEnd' => 4]; //optional cell range
-        $worksheetCellList = $spreadsheetAPI->getWorksheetCellList($spreadsheetIds[0], $worksheetIds[0], $cellRange);
+        //$cellRange = ['columnStart' => 1, 'columnEnd' => 3, 'rowStart' => 1, 'rowEnd' => 5]; //optional cell range
+        $worksheetCellList = $spreadsheetAPI->getWorksheetCellList($spreadsheetIds[0], $worksheetIds[0]);//, $cellRange);
         $data['wsclist'] = $worksheetCellList;
         /*-------------------------------------------------------------------------------------------*/
 
         /*------------------ update value for selected cell (Cell A2 -> 'Kopi') ---------------------*/
-        $worksheetCellList['A2']->setValue('Kopi');
-        $spreadsheetAPI->updateWorksheetCellList($spreadsheetIds[0], $worksheetIds[0], $worksheetCellList);
+        //$worksheetCellList['A2']->setValue('Kopi');
+        //$spreadsheetAPI->updateWorksheetCellList($spreadsheetIds[0], $worksheetIds[0], $worksheetCellList);
+        /*-------------------------------------------------------------------------------------------*/
+
+        /*--------------------------------- the main code for view ----------------------------------*/
+        $sheetList = array();
+        $wsList = array();
+        foreach($spreadsheetIds as $key){
+            $sheetList[$spreadsheetList[$key]['name']] = $key;
+            $wsKeys = array_keys($spreadsheetAPI->getWorksheetList($key));
+            $wsList[$spreadsheetList[$key]['name']] = $wsKeys;
+        }
+        $data['sheetList'] = $sheetList;
+        $data['wsList'] = $wsList;
         /*-------------------------------------------------------------------------------------------*/
 
         removeTokenFile();
@@ -55,6 +67,38 @@ class Testing extends CI_Controller
         $this->layout = 'Yes';
         $this->layoutName = 'default';
         $this->load->view('lists', $data);
+        //$this->load->view('lists');
+    }
+
+    public function update()
+    {
+        $this->load->helper('sheet_api');
+        $config = createConfig();
+        loadTokenFromDB();
+        $token_data = unserialize(file_get_contents($config['tokenDataFile']));
+        $oAuthInstance = getOAuth2GoogleAPI();
+        $oAuthInstance->setTokenData(
+            $token_data['accessToken'],
+            $token_data['tokenType'],
+            $token_data['expiresAt'],
+            $token_data['refreshToken']
+        );
+        $oAuthInstance->setTokenRefreshHandler(function(array $token_data){
+            saveToken($token_data);
+        });
+        $spreadsheetAPI = createSpreadsheetAPI($oAuthInstance);
+
+        $sheetid = $this->input->post('sheetid');
+        $worksid = $this->input->post('worksid');
+        $cellid = $this->input->post('cellid');
+        $upvalue = $this->input->post('upvalue');
+
+        $worksheetCellList = $spreadsheetAPI->getWorksheetCellList($sheetid,$worksid);
+        $worksheetCellList[$cellid]->setValue($upvalue);
+        $spreadsheetAPI->updateWorksheetCellList($sheetid, $worksid, $worksheetCellList);
+
+        $this->load->helper('url');
+        redirect('Testing');
     }
 }
 ?>
--- application/views/lists.php
+++ application/views/lists.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 $this->load->helper('form');
 ?>
 <!DOCTYPE html>
@@ -9,31 +9,49 @@ $this->load->helper('form');
         <title>Homepage</title>
     </head>
     <body>
-        <p>
+        <h3>
             Below is/are spreadsheet(s) on your Drive.
-        </p>
-        <div>
-            <?php
-                print_r($sslist);
-            ?>
-        </div>
-        <br>
-        <div>
-            <?php
-                print_r($wslist);
-            ?>
-        </div>
-        <br>
-        <div>
-            <?php
-                print_r($wsdlist);
-            ?>
-        </div>
-        <br>
-        <div>
-            <?php
-                print_r($wsclist);
-            ?>
+        </h3>
+        <?php
+            foreach ($sheetList as $key => $value) {
+                $worksheets = implode(',', $wsList[$key]);
+                $string = 'Spreadsheet: <b>'.$key.'</b> with ID="<b>'.$value.'</b>"';
+                $string .= " with worksheet id's: <b>".$worksheets.'</b>';
+                echo $string;
+            }
+            echo '<br/>';
+            print_r($wsclist);
+        ?>
+        <br/><hr/>
+        <h3 class="text-center">
+            <b>
+                Update spreadsheet.
+            </b>
+        </h3>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <?php
+                echo form_open('Testing/update');
+                echo '<div class="form-group">';
+                echo form_label('Spreadsheet ID', 'sheetid');
+                echo form_input('sheetid', '', 'class="form-control" placeholder="ex. 1LX2q....."');
+                echo '</div>';
+                echo '<div class="form-group">';
+                echo form_label('Worksheet ID', 'worksid');
+                echo form_input('worksid', '', 'class="form-control" placeholder="ex. od6"');
+                echo '</div>';
+                echo '<div class="form-group">';
+                echo form_label('Cell ID', 'cellid');
+                echo form_input('cellid', '', 'class="form-control" placeholder="ex. A2"');
+                echo '</div>';
+                echo '<div class="form-group">';
+                echo form_label('Updated Value', 'upvalue');
+                echo form_input('upvalue', '', 'class="form-control" placeholder="input value here"');
+                echo '</div>';
+                echo form_submit('submit', 'Update', 'class="btn btn-primary btn-block"');
+                echo form_close();
+                ?>
+            </div>
         </div>
     </body>
 </html>
